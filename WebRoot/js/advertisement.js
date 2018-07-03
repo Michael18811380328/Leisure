40ac934a7f676feb886462b3a12dcb0f65e06664
MichaelAn <37589122+Michael18811380328@users.noreply.github.com>
2018-07-03 23:02:14 +0800

    Create advertisement.js

---
+++ WebRoot/js/advertisement.js
@@ -0,0 +1,277 @@
+/*--------百度统计
+function  baidu_stat(){
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write("<div id='baidu_stat_id' style='display:none;'>");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F5caa30e0c191a1c525d4a6487bf45a9d' type='text/javascript'%3E%3C/script%3E"));
+document.write("</div>");
+}
+baidu_stat();
+document.getElementById("baidu_stat_id").style.display="none";
+document.write(unescape("%3Cscript src='http://static4.baihe.com/tongduntoken.js'  charset='utf-8'  type='text/javascript'%3E%3C/script%3E"));
+*/
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?5caa30e0c191a1c525d4a6487bf45a9d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+//--------空闲帮标准尾内容
+var bzw_html = '';
+bzw_html += '<div id="BHFooter" style="background-color:#ffc107;padding-bottom:10px;">';
+bzw_html += '<div class="cont">';
+bzw_html += '	<ul style="margin:0 auto;text-align:center;"">';
+bzw_html += '		<li style="margin:0 auto; text-align:center;">';
+bzw_html += '			<span style="text-align:center;"><a href="http://help.baihe.com/index.php?action=list&cat=568&listid=28#" target="_blank" onfocus="this.blur();">关于空闲帮</a> | <a href="http://help.baihe.com/index.php?action=list&cat=569&listid=28#" target="_blank" onfocus="this.blur();">商务合作</a> | <a href="http://help.baihe.com/index.php?action=list&cat=571&listid=28#" target="_blank" onfocus="this.blur();">联系我们</a> | <a href="http://help.baihe.com/index.php?action=list&cat=572&listid=28#" target="_blank" onfocus="this.blur();">诚聘人才</a> | <a href="http://help.baihe.com/" target="_blank" onfocus="this.blur();">帮助中心</a><a href="http://help.baihe.com/index.php?action=list&cat=573&listid=28#" target="_blank" onfocus="this.blur();">服务条款</a> | <a href="http://help.baihe.com/index.php?action=list&cat=574&listid=28#" target="_blank" onfocus="this.blur();">隐私保护</a> | <a href="http://safe.baihe.com" target="_blank" onfocus="this.blur();">交友安全</a> | <a href="javaScript:void(1)" onclick="openChat()"  onfocus="this.blur();">意见反馈</a> | <a href="http://www.baihe.com/sitemap.html" target="_blank">网站地图</a><a href="http://help.baihe.com/index.php?action=list&cat=633&listid=3" target="_blank" onfocus="this.blur();">家长监护工程</a> | <a href="http://music.baihe.com" target="_blank">空闲帮直播</a> | <a href="http://game.baihe.com" target="_blank">空闲帮游戏</a></span><BR />客服微信：baihekefu';
+bzw_html += '		</li>';
+bzw_html += '		<li style="margin:0 auto;text-align:center;padding:10px 0px;">';
+bzw_html += '			<span>北京市朝阳区阜通东大街1号院望京SOHO 塔3 B座 6层(100102)<br />';
+bzw_html += '           服务城市：<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">北京</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">上海</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">广州</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">深圳</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">天津</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">太原</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">成都</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">哈尔滨</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">大连</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">合肥</a>|';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">杭州</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">济南</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">南京</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">青岛</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">武汉</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">西安</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">郑州</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">石家庄</a> | ';
+bzw_html += '			<a target="_blank" href="http://vip.baihe.com/?from=baihe&site=foot">更多</a>' ;
+bzw_html += '			</span><br />';
+bzw_html += '			违法和不良信息举报电话：0316-5266032（24小时）<br />';
+bzw_html += '		</li>';
+bzw_html += '	</ul>';
+bzw_html += '	<div class="clear"></div>';
+bzw_html += '	<p style="color:#333; font-family:Arial" ><a style="text-decoration: none;" href="http://www.miibeian.gov.cn" target="_blank">京ICP备10009806号-1</a>　　京公网安备110105000655号　　京ICP证041124号　　京网文（2016）6588-901号　　版权所有&copy; 2005 - '+new Date().getFullYear()+' 空闲帮网<br />';
+bzw_html += '	<a target="_blank" href="http://media.baihe.com/index.php?action-viewnews-itemid-771-php-1">广播电视节目制作经营许可证（京）字第01303号</a></p>';
+bzw_html += '<hr style="width:80%;color:#fff;" />';
+bzw_html += '</div>';
+bzw_html += '</div>';
+
+document.write(bzw_html);
+
+//--------意见反馈
+function openChat(){
+	if(NTKF_PARAM.userlevel){
+		NTKF.im_openInPageChat('kf_9847_1390382637068');
+	}else{
+		NTKF.im_openInPageChat('kf_9847_1386825539485');
+	}
+}
+NTKF_PARAM = {
+  siteid:"kf_9847",
+  settingid: "kf_9847_1386825539485",
+  uid:getFootCookie("GCUserID"),
+  uname:getFootCookie("GCEmail"),
+  userlevel:getFootCookie("KeFuVip")==""?0:getFootCookie("KeFuVip")
+} 
+
+//document.write('<script type=\"text/javascript\" src=\"http://dl.ntalker.com/js/xn6/ntkfstat.js?siteid=kf_9847\" charset=\"utf-8\"></script>');
+
+function getFootCookie(NameOfCookie){
+	if (document.cookie.length > 0) {
+		begin = document.cookie.indexOf(NameOfCookie+"=");
+		if (begin != -1){
+			begin += NameOfCookie.length+1;//cookie值的初始位置
+			end = document.cookie.indexOf(";", begin);//结束位置
+			if (end == -1) end = document.cookie.length;//没有;则end为字符串结束位置
+				return unescape(document.cookie.substring(begin, end));
+			}
+		}
+	return "";
+} 
+
+if(typeof(bzt_headType)!='undefined'&&bzt_headType == 0){
+	if(!bzt_isMainUser){
+		//	writeGoogleCode_indexPage(); //google codes
+	}
+}
+
+
+
+
+//--------report 统计
+
+var locurlPre="";
+var _baiheHead_ready_time = new Date();
+var locUrlSrc = location.href;
+var num=locUrlSrc.indexOf("?");
+var start=locUrlSrc.indexOf("http://");
+var referurl = document.referrer;
+var fromURL="";
+var fromURLParam="";
+var rfstart =referurl.indexOf("http://");
+var ggCode="";
+var beViewedUserId="";
+var num1=referurl.indexOf("?");
+ var img = new Image();
+ var loadTime = 0;
+ if(typeof(_baiheHead_start_time)!='undefined' && _baiheHead_start_time){
+	loadTime=_baiheHead_ready_time-_baiheHead_start_time;
+}
+
+if(num > 0){
+	locurlPre=locUrlSrc.substr(num+1)+"";
+	var _num = locurlPre.indexOf("#");
+	
+	if(_num > 0){
+			locurlPre = locurlPre.substring(0,_num);
+	}
+}else{
+	locurlPre = "";
+}
+	
+if(num>=0){
+	 var locURL=locUrlSrc.substring(start,num);	
+}
+ else{
+	 locURL=locUrlSrc;
+}
+	
+if(num1>0){
+	fromURL=referurl.substring(rfstart,num1);
+                     
+}else{
+	fromURL=referurl;
+}
+
+if(num1>0){
+	fromURL=referurl.substring(rfstart,num1);
+	getBhtg_ggcode();
+}else{
+	fromURL=referurl;
+}
+
+function getBhtg_ggcode(){
+
+	fromURLParam=referurl.substr(num1+1)+"";
+	if(fromURLParam!=null&&fromURLParam!=''){
+			var fromParams = fromURLParam.split("&");
+			for(var i = 0 ; i < fromParams.length ; i ++){
+					var p = fromParams[i];    
+				  var p1 = 	p.split("=")
+					if(ggCode==''&& p1[0] == "ggCode"){
+							ggCode = p1[1];
+					}
+					if(p1[0] == "rvCode"){
+							ggCode = p1[1];
+					}
+			}
+	}
+	if(ggCode.length>0){
+	  var	ggCodeEIdx=ggCode.indexOf("_");
+		beViewedUserId=ggCode.substr(ggCodeEIdx+1)+"";
+		ggCode=ggCode.substring(0,ggCodeEIdx)+"";
+	}
+	/*var ggCodeBIdx=fromURLParam.indexOf("ggCode=");
+	var rvCodeBIdx=fromURLParam.indexOf("rvCode=");
+	var ggCodeEIdx=fromURLParam.indexOf("#");
+	if(ggCodeBIdx>=0 && ggCodeEIdx>0){
+		ggCode=fromURLParam.substring(ggCodeBIdx+7,ggCodeEIdx)+"";
+	}
+	if(ggCodeBIdx>=0){
+		ggCode=fromURLParam.substr(ggCodeBIdx+7)+"";
+	}
+	if(ggCode.length>0){
+		ggCodeEIdx=ggCode.indexOf("_");
+		beViewedUserId=ggCode.substr(ggCodeEIdx+1)+"";
+		ggCode=ggCode.substring(0,ggCodeEIdx)+"";
+	}*/     
+	 
+}
+
+function bzw_getCookieVal(offset) {
+    var endstr = document.cookie.indexOf (";", offset);
+    if (endstr == -1) {
+        endstr = document.cookie.length;
+    }
+    return unescape(document.cookie.substring(offset, endstr));
+}
+
+// primary function to retrieve cookie by name
+function bzw_getCookie(name) {
+    var arg = name + "=";
+    var alen = arg.length;
+    var clen = document.cookie.length;
+    var i = 0;
+    while (i < clen) {
+        var j = i + alen;
+        if (document.cookie.substring(i, j) == arg) {
+            return bzw_getCookieVal(j);
+        }
+        i = document.cookie.indexOf(" ", i) + 1;
+        if (i == 0) break; 
+    }
+    return "";
+}
+
+// store cookie value with optional details as needed
+function bzw_setCookie(name, value, expires, path, domain, secure) {
+    document.cookie = name + "=" + escape (value) +
+        ((expires) ? "; expires=" + expires : "") +
+        ((path) ? "; path=" + path : "") +
+        ((domain) ? "; domain=" + domain : "") +
+        ((secure) ? "; secure" : "");
+}
+
+// remove the cookie by setting ancient expiration date
+function bzw_deleteCookie(name,path,domain) {
+    if (bzw_getCookie(name)) {
+        document.cookie = name + "=" +
+            ((path) ? "; path=" + path : "") +
+            ((domain) ? "; domain=" + domain : "") +
+            "; expires=Thu, 01-Jan-70 00:00:01 GMT";
+    }
+}
+
+// utility function to retrieve an expiration date in proper
+// format; pass three integer parameters for the number of days, hours,
+// and minutes from now you want the cookie to expire (or negative
+// values for a past date); all three parameters are required,
+// so use zeros where appropriate
+function bzw_getExpDate(days, hours, minutes) {
+    var expDate = new Date();
+    if (typeof days == "number" && typeof hours == "number" && 
+        typeof hours == "number") {
+        expDate.setDate(expDate.getDate( ) + parseInt(days));
+        expDate.setHours(expDate.getHours( ) + parseInt(hours));
+        expDate.setMinutes(expDate.getMinutes( ) + parseInt(minutes));
+        return expDate.toGMTString( );
+    }
+}
+
+
+var _ck_pcc = bzw_getCookie("cookie_pcc");
+
+var pcc_userId = bzw_getCookie("GCUserID");
+/*if(pcc_userId != ''){
+	var logTimes = bzw_getCookie(pcc_userId+"_log");
+	if(logTimes == '' || logTimes == null || typeof(pcc_userId) == 'undefined'){
+		var stronline = "<div align=center style='display:none;'><script language='javascript' src='http://log.baihe.com/'></script></div>";
+		document.write(eval("stronline"));
+		bzw_setCookie(pcc_userId+"_log", '1', -1, "/", "baihe.com", "");
+	}
+}*/
+
+if(pcc_userId == ""){
+	pcc_userId = "0";
+}
+var pcc_tmpId = bzw_getCookie("tempID");
+if(pcc_tmpId == "" || pcc_tmpId.length<10){
+	var cur_timestamp=new Date().getTime()+"";
+	pcc_tmpId = Math.round(Math.random()*100000000)+1+"";
+	pcc_tmpId=cur_timestamp.substr(cur_timestamp.length+pcc_tmpId.length-11,10-pcc_tmpId.length)+pcc_tmpId;
+	bzw_setCookie("tempID",pcc_tmpId,bzw_getExpDate(3650,0,0),"/","baihe.com");
+}
+
+/*img.src="http://report1.baihe.com/qudao.html?questStr="+locurlPre+"&&locURL="+locURL+"&Referer="+fromURL+"&loadTime="+loadTime+"&ck_pcc="+escape(_ck_pcc)+"&GCUserID="+pcc_userId+"&tempId="+pcc_tmpId+"&ggCode="+ggCode+"&BVUserID="+beViewedUserId;*/
